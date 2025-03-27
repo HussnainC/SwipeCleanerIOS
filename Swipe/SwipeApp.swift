43435d8f2eefec378011ad91f5276f1df61295b4
HussnainC <hussnaincodec@gmail.com>
2025-03-27 13:17:46 +0500

    Initial Commit

---
+++ Swipe/SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeApp.swift
+//  Swipe
+//
+//  Created by Hussnain on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
