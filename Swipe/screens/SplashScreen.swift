9af83a70538bcae14790bb99af25d287652f54e0
HussnainC <hussnaincodec@gmail.com>
2025-03-27 14:24:52 +0500

    Main

---
+++ Swipe/screens/SplashScreen.swift
@@ -0,0 +1,21 @@
+//
+//  SplashScreen.swift
+//  Swipe
+//
+//  Created by Hussnain on 27/03/2025.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        ZStack {
+            Text("Splash View")
+        }
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
+
